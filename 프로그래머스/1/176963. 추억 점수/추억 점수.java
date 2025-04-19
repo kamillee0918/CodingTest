c23e078893de19f81935ba22cc69807aa526ac90
Minjae Lee <parousia0918@gmail.com>
2025-04-20 00:10:44 +0900

    [level 1] Title: 추억 점수, Time: 2.61 ms, Memory: 91.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String, Integer> scoreMap = new HashMap<>();
+        for (int i = 0; i < name.length; i++) {
+            scoreMap.put(name[i], yearning[i]);
+        }
+        
+        // 사진별 추억 점수 계산
+        int[] answer = new int[photo.length];
+        for (int i = 0; i < photo.length; i++) {
+            int sum = 0;
+            for (String person : photo[i]) {
+                sum += scoreMap.getOrDefault(person, 0);
+            }
+            answer[i] = sum;
+        }
+        
+        return answer;
+    }
+}
